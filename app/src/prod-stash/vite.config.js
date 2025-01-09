f9fcff346eb33c4dc05df29e9f0e32b92516db5a
khomboljuice <secretprojection@gmail.com>
2025-01-09 12:29:00 +0000

    [ADDS] production files stash to laravel template

---
+++ app/src/prod-stash/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+        }),
+    ],
+    base:"/prod/",
+    prod: {
+        outDir: "public/prod",
+        emptyOutDir:true,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return 'vendor';
+                    }
+                },
+            },
+        },
+    }
+});
