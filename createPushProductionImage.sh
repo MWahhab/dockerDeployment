41696b502e944adc5ef929cfd99c86ac3a590dc5
khomboljuice <abz619ygm@gmail.com>
2025-01-13 10:50:27 +0000

    [REFACTORS] shell script and makes it easier to use

--- createPushProductionImage.sh
+++ createPushProductionImage.sh
@@ -12,16 +12,38 @@ else
     exit 1
 fi
 
-read -p "Do you want to be provided with a custom docker-compose.yml file for accessing the production image once it has been pushed? (DISCLAIMER - You may need to make changes to it based on the platform being using to host it!) (y/n): " confirmation
+read -p "Do you want to be provided with a custom docker-compose.yml file for accessing the production application once it has been pushed? (DISCLAIMER - You may need to make changes to it based on the platform being using to host it!) (y/n): " confirmation
  if [ "$confirmation" = "y" ]; then
    PROVIDE_DOCKER_COMPOSE="true"
  fi
 
-read -p "Enter name of docker deployment environment (dockerDeployment by default): " docker_environment_name
+read -p "Are you hosting this production application using HTTPS? (y/n): " isHostingUsingHttps
+
+if [ "$isHostingUsingHttps" = "y" ]; then
+  read -p "Since you're using HTTPS, please provide the URL that you will be used : " HTTPSURL
+fi
+
+read -p "Do you want to specify a platform? (y/n): " changePlatform
+
+if [ "$changePlatform" = "y" ]; then
+  read -p "Please enter the platform. (e.g: linux/arm64): " platformForBuild
+fi
 
 read -p "Enter Docker Username: " docker_username
 
 read -p "Enter image tag: " image_tag
+
+if [ "$isHostingUsingHttps" = "y" ]; then
+  #updating APP_URL in the .env file to https url
+  ENV_FILE="app/prod-stash/.env"
+  if [ -f "./$ENV_FILE" ]; then
+    ORIGINAL_APP_URL=$(grep -E '^APP_URL=' "./$ENV_FILE" | cut -d '=' -f2)
+    sed -i.bak -E "s|^APP_URL=.*|APP_URL=$HTTPSURL|" "./$ENV_FILE"
+    echo "APP_URL temporarily updated to $HTTPSURL in ./$ENV_.FILE."
+  else
+    echo "No .env file found in prod-stash directory."
+  fi
+fi
 
 cd "./$SRC_DIR"
 
@@ -30,50 +52,65 @@ if [ -d "$SRC_DIR/node_modules" ]; then
     sudo rm -rf "$SRC_DIR/node_modules"
 fi
 
-### Replace lines with #BOOT comment attached if youre using https, as otherwise http will be forced
+if [ "$isHostingUsingHttps" != "y" ]; then
+  #im forcing http here, so there arent issues with fetching the assets in the "public/build" dir. attempts to fetch using https otherwise!
+  APP_SERVICE_PROVIDER="app/Providers/AppServiceProvider.php"
+  ORIGINAL_APP_SERVICE_PROVIDER="$APP_SERVICE_PROVIDER.bak"
 
-# BOOT
-APP_SERVICE_PROVIDER="/var/www/html/$docker_environment_name/$SRC_DIR/app/Providers/AppServiceProvider.php"
-ORIGINAL_APP_SERVICE_PROVIDER="$APP_SERVICE_PROVIDER.bak"
+  cp "$APP_SERVICE_PROVIDER" "$ORIGINAL_APP_SERVICE_PROVIDER"
 
-# BOOT
-cp "$APP_SERVICE_PROVIDER" "$ORIGINAL_APP_SERVICE_PROVIDER"
+  sed -i "/public function boot()/, /}/c\
+  public function boot(): void {\n\
+      if (config('app.env') === 'production') {\n\
+          URL::forceScheme('http');\n\
+      }\n\
+  " "$APP_SERVICE_PROVIDER"
+fi
 
-
-# BOOT
-sed -i "/public function boot()/, /}/c\
-public function boot(): void {\n\
-    if (config('app.env') === 'production') {\n\
-        URL::forceScheme('http');\n\
-    }\n\
-" "$APP_SERVICE_PROVIDER"
-
-
+#reading the original build command so i can change it to npx vite build --mode production
 BUILD_KEY="build"
-PACKAGE_JSON="/var/www/html/$docker_environment_name/$SRC_DIR/package.json"
-ORIGINAL_BUILD_COMMAND=$(grep -oP '"build"\s*:\s*"\K[^"]+' "$PACKAGE_JSON")  # Read the original "build" command
+PACKAGE_JSON="package.json"
+ORIGINAL_BUILD_COMMAND=$(grep -oP '"build"\s*:\s*"\K[^"]+' "$PACKAGE_JSON")
 
 sed -i.bak -E "s|(\"build\":\s*\")[^\"]+|\1npx vite build --mode production|" "$PACKAGE_JSON"
 
 cd ../..
 
 echo "Building Docker images with tag $image_tag..."
-docker build --build-arg PROD=true --no-cache -t $docker_username/apache-prod:$image_tag -f apache-custom/Dockerfile .
-docker build --build-arg PROD=true --no-cache -t $docker_username/node-prod:$image_tag -f node-custom/Dockerfile .
-docker build --build-arg PROD=true --no-cache -t $docker_username/php-fpm-prod:$image_tag -f php-fpm-custom/Dockerfile .
-docker build --build-arg PROD=true --no-cache -t $docker_username/php-cli-prod:$image_tag -f php-cli-custom/Dockerfile .
+
+if [ "$changePlatform" = "y" ]; then
+    echo "Using platform $platformForBuild for Docker builds."
+    docker build --build-arg PROD=true --no-cache --platform "$platformForBuild" -t $docker_username/apache-prod:$image_tag -f apache-custom/Dockerfile .
+    docker build --build-arg PROD=true --no-cache --platform "$platformForBuild" -t $docker_username/node-prod:$image_tag -f node-custom/Dockerfile .
+    docker build --build-arg PROD=true --no-cache --platform "$platformForBuild" -t $docker_username/php-fpm-prod:$image_tag -f php-fpm-custom/Dockerfile .
+    docker build --build-arg PROD=true --no-cache --platform "$platformForBuild" -t $docker_username/php-cli-prod:$image_tag -f php-cli-custom/Dockerfile .
+else
+    docker build --build-arg PROD=true --no-cache -t $docker_username/apache-prod:$image_tag -f apache-custom/Dockerfile .
+    docker build --build-arg PROD=true --no-cache -t $docker_username/node-prod:$image_tag -f node-custom/Dockerfile .
+    docker build --build-arg PROD=true --no-cache -t $docker_username/php-fpm-prod:$image_tag -f php-fpm-custom/Dockerfile .
+    docker build --build-arg PROD=true --no-cache -t $docker_username/php-cli-prod:$image_tag -f php-cli-custom/Dockerfile .
+fi
 
 if [ $? -ne 0 ]; then
     echo "Error building Docker images. Please check the logs and try again."
-    # BOOT
     cp "$ORIGINAL_APP_SERVICE_PROVIDER" "$APP_SERVICE_PROVIDER"
     exit 1
 fi
 
-# BOOT
-cp "$ORIGINAL_APP_SERVICE_PROVIDER" "$APP_SERVICE_PROVIDER"
+if [ "$isHostingUsingHttps" = "y" ]; then
+  cp "$ENV_FILE.bak" "$ENV_FILE"
+  rm -f "$ENV_FILE.bak"
+fi
+
+cd app/src
+
+if [ "$isHostingUsingHttps" != "y" ]; then
+  cp "$ORIGINAL_APP_SERVICE_PROVIDER" "$APP_SERVICE_PROVIDER"
+  rm -f "$APP_SERVICE_PROVIDER.bak"
+fi
 
 sed -i -E "s|(\"build\":\s*\")[^\"]+|\1${ORIGINAL_BUILD_COMMAND}|" "$PACKAGE_JSON"
+rm -f "$PACKAGE_JSON.bak"
 
 echo "Tagging and pushing images to Docker Hub for user $docker_username..."
 docker push $docker_username/apache-prod:$image_tag
