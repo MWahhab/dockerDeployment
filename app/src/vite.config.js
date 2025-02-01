7d4224cae3a6c0c9f950b4da443130b700f7a2b4
khomboljuice <abz619ygm@gmail.com>
2025-02-01 20:31:17 +0000

    [FIXES] setup issues and pre-installs breeze for ease of use

--- app/src/vite.config.js
+++ app/src/vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: "0.0.0.0",
+        port: 5173,
+        origin: "http://localhost:5173",
+        cors: true,
+        hmr: {
+            host: "localhost",
+            port: 5173,
+            protocol: "ws"
+        }
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
+        react(),
     ],
 });
